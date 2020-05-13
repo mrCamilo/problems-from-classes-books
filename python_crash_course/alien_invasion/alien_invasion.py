94f8678edc7366b6e9e5347fe7f3c15333ff2dd9
Manuel <mcamilo@protonmail.com>
2020-05-13 18:48:24 -0400

    settings file

--- python_crash_course/alien_invasion/alien_invasion.py
+++ python_crash_course/alien_invasion/alien_invasion.py
@@ -1,27 +1,38 @@
-#Pygame window, responding to userinput
+# Pygame window, responding to user input
 
 import sys
-
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
-#Overall class to manage game assets and behavior.
+    """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+                (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         def run_game(self):
-            #Start the main loop for the game
+            """Start the main loop for the game"""
             while True:
                 # Watch for keyboard and mouse events
                 for event in pygame.event.get():
-                    if event.type == pygame.QUIT:                    sys.exit()
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+                self.screen.fill(self.settings.bg_color)
 
-            # Make the most recently drawn screen visible.z             pygame.display.flip()
+            #Redraw screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
             if __name__ == '__main__':
-                # Make a game instance, and run the game.    ai = AlienInvasion()
+                # Make a game instance, and run the game.
+                ai = AlienInvasion()
                 ai.run_game()
