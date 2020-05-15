7576d8a7430ae88ba78e0c696e2b578a2173d334
Manuel <mcamilo@protonmail.com>
2020-05-15 06:55:35 -0400

    movement flags

--- python_crash_course/alien_invasion/ship.py
+++ python_crash_course/alien_invasion/ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize ship and set starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load ship image and rect
@@ -14,6 +15,22 @@ class Ship:
 
         #start each new ship at bottom center of screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #store ship horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update position from moevement flags"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw ship at current location"""
