6a50d38cebfc60ff95ac099120391801c5a6d7d6
Manuel <mcamilo@protonmail.com>
2020-05-16 06:01:15 -0400

    bullets class

--- python_crash_course/alien_invasion/bullet.py
+++ python_crash_course/alien_invasion/bullet.py
@@ -2,14 +2,16 @@ import pygame
 from pygame.sprite import Sprite
 
 class Bullet(Sprite):
-    def __init__(self_ai_game):
+    """Manage bullets fired from ship"""
+    
+    def __init__(self, ai_game):
         super().__init__()
-        self.screen=ai_game.screen
-        self.settings=ai_game.settings
-        self.color=self.settings.bullet_color
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color= self.settings.bullet_color
 
-        self.rect=pygame.Rect(0.0, self.settings.bullet_width,
-                self.settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
         self.y = float(self.rect.y)
