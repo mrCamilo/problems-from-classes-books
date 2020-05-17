b44731ce25572c4585a0190262f5e3b0bccf55df
Manuel <mcamilo@protonmail.com>
2020-05-17 06:16:40 -0400

    settings including screen size

--- python_crash_course/alien_invasion/settings.py
+++ python_crash_course/alien_invasion/settings.py
@@ -8,17 +8,19 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
+        # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
-        #bullets
-        self.bullet_speed = 1.0
+        # Bullet settings
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
-        #aliens
+        # Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        #fleet direction of 1 represents right; -1 represents left
+        # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
