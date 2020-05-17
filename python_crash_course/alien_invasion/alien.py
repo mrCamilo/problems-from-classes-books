f165f13edb8ae969ed8e4a63be24f6460977ef78
Manuel <mcamilo@protonmail.com>
2020-05-17 15:15:25 -0400

    enemies

--- python_crash_course/alien_invasion/alien.py
+++ python_crash_course/alien_invasion/alien.py
@@ -2,8 +2,6 @@ import pygame
 from pygame.sprite import Sprite
  
 class Alien(Sprite):
-    """A class to represent a single alien in the fleet."""
-
     def __init__(self, ai_game):
         """Initialize the alien and set its starting position."""
         super().__init__()
@@ -22,13 +20,12 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
 
     def check_edges(self):
-        """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
         if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
 
     def update(self):
-        """Move the alien right or left."""
+        """alien goes right or left."""
         self.x += (self.settings.alien_speed *
                         self.settings.fleet_direction)
         self.rect.x = self.x
