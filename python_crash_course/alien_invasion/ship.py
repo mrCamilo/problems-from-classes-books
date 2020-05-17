786dbba96d6b9aa2b57378e45b0193178314dc3c
Manuel <mcamilo@protonmail.com>
2020-05-17 06:15:42 -0400

    userinput

--- python_crash_course/alien_invasion/ship.py
+++ python_crash_course/alien_invasion/ship.py
@@ -1,22 +1,22 @@
 import pygame
-
+ 
 class Ship:
-    """Manages ship"""
-
+    """A class to manage the ship."""
+ 
     def __init__(self, ai_game):
-        """Initialize ship and set starting position"""
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        #Load ship image and rect
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        #start each new ship at bottom center of screen
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        #store ship horizontal position
+        # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
         # Movement flags
@@ -24,14 +24,21 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        """update position from moevement flags"""
+        """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
+        # Update rect object from self.x.
         self.rect.x = self.x
 
     def blitme(self):
-        """Draw ship at current location"""
+        """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
