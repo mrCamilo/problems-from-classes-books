d0ca9ed551a59d257a35932d95cc44a35579f57c
mrCamilo <mcamilo@protonmail.com>
2020-05-20 06:19:11 -0400

    frequency of 100 simulated dice rolls using matplotlib

--- python_crash_course/data_visualization/die_visual.py
+++ python_crash_course/data_visualization/die_visual.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as mpl
 from die import Die
 
 #d6
@@ -10,4 +11,17 @@ for roll_num in range(100):
     result=die.roll()
     results.append(result)
 
-print (results)
+# Analyze results
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#Bar graph here!! Show frequencies ^_^
+mpl.hist(results, 50)
+
+mpl.xlabel("Result")
+mpl.ylabel("Frequency of Result")
+mpl.title("Rolling a Die One Hundred times")
+
+mpl.show()
