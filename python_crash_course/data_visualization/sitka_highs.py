399fe8b01d00e3ee7d36e092281cba60e9d489d5
mrCamilo <mcamilo@protonmail.com>
2020-05-21 05:40:23 -0400

    includes dates on X axis

--- python_crash_course/data_visualization/sitka_highs.py
+++ python_crash_course/data_visualization/sitka_highs.py
@@ -1,20 +1,21 @@
 import csv
-
+from datetime import datetime
 import matplotlib.pyplot as mplpp
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    #print (header_row)
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
     # High temperatures
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
 
     #Low temperatures
@@ -26,11 +27,12 @@ with open(filename) as f:
     #plot
     mplpp.style.use('seaborn')
     fig, ax = mplpp.subplots()
-    ax.plot(highs, c='red')
+    ax.plot(dates, highs, c='red')
 
     #plot format
     mplpp.title("Daily highs, July 2018", fontsize=24)
     mplpp.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
     mplpp.ylabel("Temperature (F)", fontsize=16)
     mplpp.tick_params(axis='both', which='major', labelsize=16)
 
