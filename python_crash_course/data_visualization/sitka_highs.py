a679f82bd1c6a45e1cfd97026fdc6cec1d64abb1
mrCamilo <mcamilo@protonmail.com>
2020-05-21 05:33:09 -0400

    missing data and time

--- python_crash_course/data_visualization/sitka_highs.py
+++ python_crash_course/data_visualization/sitka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as mplpp
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -8,3 +10,30 @@ with open(filename) as f:
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
+
+    # High temperatures
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+    #Low temperatures
+    #lows = []
+    #for row in reader:
+        #   low = int(row[6])
+        #  lows.append(low)
+
+    #plot
+    mplpp.style.use('seaborn')
+    fig, ax = mplpp.subplots()
+    ax.plot(highs, c='red')
+
+    #plot format
+    mplpp.title("Daily highs, July 2018", fontsize=24)
+    mplpp.xlabel('', fontsize=16)
+    mplpp.ylabel("Temperature (F)", fontsize=16)
+    mplpp.tick_params(axis='both', which='major', labelsize=16)
+
+mplpp.show()
+
+#print(highs)
