5fbbf05169c164a6329a97dbdea00a9c3037cf01
mrCamilo <mcamilo@protonmail.com>
2020-06-15 11:57:53 -0400

    MemeGenerator and a Header

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+   return(
+      <h1>Header goes here.</h1>
+   )
+}
+
+export default Header
---
+++ meme-generator/src/components/MemeGenerator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MemeGenerator() {
+    return(
+        <h3>Don't say goodbye.... smell you later...</h3>
+    )
+}
+
+export default MemeGenerator
